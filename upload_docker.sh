878840872dd0ef5bad56458948eca847d23d97ea
Folasade <fadedeji@gmail.com>
2020-08-19 22:30:50 +0100

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,18 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
 dockerpath="folasade/capstonefola"
 
 # Step 2:  
 # Authenticate & tag
-docker login -u folasade --p Folasade1!
-docker login
-docker tag capstonefola $dockerpath
-
 echo "Docker ID and Image: $dockerpath"
-
+# docker login --username folasade
+docker image tag capstonefola $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker push folasade/capstonefola:latest
+docker push $dockerpath
